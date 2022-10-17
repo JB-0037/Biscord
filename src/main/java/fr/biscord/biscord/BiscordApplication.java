1d7bae2a8ea3284b49712d76daa42ddb497246cb
JB-0037 <daulaus@et.intechinfo.fr>
2022-10-17 15:06:54 +0200

    first commit

---
+++ src/main/java/fr/biscord/biscord/BiscordApplication.java
@@ -0,0 +1,13 @@
+package fr.biscord.biscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscordApplication.class, args);
+	}
+
+}
